bd1462b1d39885111995834c14023daf24e68526
Carl Henrique <carlhenriquedev@gmail.com>
2026-07-05 12:14:12 -0300

    exportei o banco de dados para o modulo de sorteio

--- functions/sortearNumeros.js
+++ functions/sortearNumeros.js
@@ -1,3 +1,5 @@
+const { database } = require("../db/database.js"); // exportando a funcao de arrays do bingo
+
 export function sortearNumeros() {
     // lógica para sorteio e apresentação do número sorteado..
     
