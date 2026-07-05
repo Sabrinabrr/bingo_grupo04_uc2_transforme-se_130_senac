bceb97a7960feb7bb9be913cbcf3dbf20eb93fee
Sabrina <sabrinabarros0102@gmail.com>
2026-07-05 10:57:17 -0300

    correção da variavel

--- db/database.js
+++ db/database.js
@@ -1,17 +1,17 @@
 // Banco de dados
 export function database(){
 // inicio dos vetores do sistema
-    let numerosDisponivel = [];
+    let numerosDisponiveis = [];
     let numerosSorteados = [];
     let ultimoNumeroSorteado = null;
 
-    numerosDisponivel = [];
+    numerosDisponiveis = [];
     numerosSorteados = [];
     ultimoNumeroSorteado = null;
     
      // inicinado o laço de repetição para os numeros disponivel 
 
      for (let i = 1; i <= 75; i++) {
-        numerosDisponivel.push(i);
+        numerosDisponiveis.push(i);
     }
 }
