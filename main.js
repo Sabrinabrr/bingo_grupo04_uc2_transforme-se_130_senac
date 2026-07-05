bbb77656984e41479bcd7aad8b8d29694425d518
Carlos Henrique <carlhenriquedev@gmail.com>
2026-07-05 11:43:49 -0300

    adicionei as opcoes do menu visualmente no terminal

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const { sortearNumeros } = require("./functions/sortearNumeros.js");
 const { mostrarHistorico } = require("./functions/mostrarHistorico.js");
 const { pesquisarNumero } = require("./functions/pesquisarNumero.js");
 const { mostrarRegistroSistema } = require("./functions/mostrarRegistroSistema.js");
+const { reiniciarBingo } = require("./functions/reiniciarBingo.js");
 
 // importando biblioteca para receber dados no terminal
 const readline = require("readline");
@@ -14,12 +15,18 @@ const rl = readline.createInterface({
 });
 
 // inicializando o sistema
+console.log("==================");
+console.log("BEM VINDO AO BINGO");
+
 function menu() {
     
-    console.log("==================");
-    console.log("BEM VINDO AO BINGO\n");
-    
-    rl.question("Escolha uma opção: ", function(escolha){
+    console.log("\n[1] Sortear números");
+    console.log("[2] Mostrar histórico");
+    console.log("[3] Pesquisar se número foi chamado");
+    console.log("[4] Mostrar registro do sistema");
+    console.log("[5] Reiniciar bingo");
+    console.log("[0] Encerrar o programa");
+    rl.question("\nEscolha uma opção: ", function(escolha){
     
         if(escolha == 1){
             sortearNumeros();
@@ -38,7 +45,12 @@ function menu() {
             menu(); // reiniciando o menu
             
         } else if(escolha == 5) {
-            console.log("encerrar o programa!");
+            console.log("Bingo atual encerrado!");
+            reiniciarBingo();
+            menu(); // reiniciando o menu
+        
+        } else if(escolha == 0) {
+            console.log("Fim algoritmo!");
             
         } else {
             console.log("escolha inválida!");
