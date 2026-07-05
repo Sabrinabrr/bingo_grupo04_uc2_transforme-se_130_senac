ecb83972ae0e7339f9e69dfeb1fc22bb55a8486a
Carlos Henrique <carlhenriquedev@gmail.com>
2026-07-05 11:19:46 -0300

    removendo objetos obsoletos e adicionando comentarios

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 // Menu do bingo
+
+// importando funções
 const { sortearNumeros } = require("./functions/sortearNumeros.js");
 const { mostrarHistorico } = require("./functions/mostrarHistorico.js");
 const { pesquisarNumero } = require("./functions/pesquisarNumero.js");
 const { mostrarRegistroSistema } = require("./functions/mostrarRegistroSistema.js");
 
+// importando biblioteca para receber dados no terminal
 const readline = require("readline");
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// inicializando o sistema
 function menu() {
-    let escolha;
     
     console.log("==================");
     console.log("BEM VINDO AO BINGO\n");
@@ -21,28 +23,28 @@ function menu() {
     
         if(escolha == 1){
             sortearNumeros();
-            menu();
+            menu(); // reiniciando o menu
             
         } else if(escolha == 2) {
             mostrarHistorico();
-            menu();
+            menu(); // reiniciando o menu
             
         } else if(escolha == 3) {
             pesquisarNumero();
-            menu();
+            menu(); // reiniciando o menu
             
         } else if(escolha == 4) {
             mostrarRegistroSistema();
-            menu();
+            menu(); // reiniciando o menu
             
         } else if(escolha == 5) {
             console.log("encerrar o programa!");
             
         } else {
             console.log("escolha inválida!");
-            menu();
+            menu(); // reiniciando o menu
         }
     });
 }
 
-menu();
+menu(); // chamando função
