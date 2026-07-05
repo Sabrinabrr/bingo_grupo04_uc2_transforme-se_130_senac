9d23b6a412ccbef78b8aa76cc73accfcbd042523
Sabrina <sabrinabarros0102@gmail.com>
2026-07-05 10:55:24 -0300

    Banco de dados atualizado

--- db/database.js
+++ db/database.js
@@ -1,1 +1,17 @@
 // Banco de dados
+export function database(){
+// inicio dos vetores do sistema
+    let numerosDisponivel = [];
+    let numerosSorteados = [];
+    let ultimoNumeroSorteado = null;
+
+    numerosDisponivel = [];
+    numerosSorteados = [];
+    ultimoNumeroSorteado = null;
+    
+     // inicinado o laço de repetição para os numeros disponivel 
+
+     for (let i = 1; i <= 75; i++) {
+        numerosDisponivel.push(i);
+    }
+}
