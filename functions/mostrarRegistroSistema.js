648451e836a12fa8f66ad5c134d828ae269586b1
Caio Cesar <caiocesarc200@gmail.com>
2026-07-06 18:46:16 -0300

    feat: funcao de registo e de historico adicionadas

--- functions/mostrarRegistroSistema.js
+++ functions/mostrarRegistroSistema.js
@@ -3,7 +3,7 @@ const { database } = require("../db/database.js");
 function mostrarRegistroSistema() {
     console.log("Quantidade de números disponíveis: ", database.numerosDisponiveis.length);
     console.log("Quantidade de números sorteados: ", database.numerosSorteados.length);
-    console.log("Quantidade de números total: ", database.numerosDisponiveis.length + database.numerosSorteados.length);
+    console.log("Quantidade de números total: ", database.numerosDisponiveis.length + database.numerosSorteados.length)
 }
 
 module.exports = {
